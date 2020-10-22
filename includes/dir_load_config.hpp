9c9d95f486f401add70f27ba81da5e313cde41c0
Can Bölük <can.boluk89@gmail.com>
2020-10-22 03:17:34 +0200

    Adding license.

--- includes/dir_load_config.hpp
+++ includes/dir_load_config.hpp
@@ -1,3 +1,30 @@
+// Copyright (c) 2020 Can Boluk
+// All rights reserved.   
+//    
+// Redistribution and use in source and binary forms, with or without   
+// modification, are permitted provided that the following conditions are met: 
+//    
+// 1. Redistributions of source code must retain the above copyright notice,   
+//    this list of conditions and the following disclaimer.   
+// 2. Redistributions in binary form must reproduce the above copyright   
+//    notice, this list of conditions and the following disclaimer in the   
+//    documentation and/or other materials provided with the distribution.   
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software 
+//    without specific prior written permission.   
+//    
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE   
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR   
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF   
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS   
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN   
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)   
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE.        
+//
 #pragma once
 #include "common.hpp"
 #include "dir_relocs.hpp"
@@ -140,8 +167,8 @@ namespace win
         uint32_t                    size;
         union
         {
-            dynamic_reloc_t<x64>    v1; // Variable length array.
-            dynamic_reloc_v2_t<x64> v2; // Variable length array.
+            dynamic_reloc_t<x64>    first_entry; // Variable length array.
+            dynamic_reloc_v2_t<x64> first_v2_entry; // Variable length array.
         };
     };
     using dynamic_reloc_table_x86_t = dynamic_reloc_table_t<false>;
